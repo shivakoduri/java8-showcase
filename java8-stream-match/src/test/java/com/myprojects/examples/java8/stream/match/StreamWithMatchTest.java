ad290dd6dfcda1f9aa69a4333db977c8c5ed0630
shivakoduri <koduri5782>
2018-04-27 15:19:50 +0800

    Initial Commit for Java8- Adding Stream Match examples

---
+++ java8-stream-match/src/test/java/com/myprojects/examples/java8/stream/match/StreamWithMatchTest.java
@@ -0,0 +1,45 @@
+package com.myprojects.examples.java8.stream.match;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamWithMatchTest {
+
+    @Test
+    public void shouldCheckIfThereIsANumberGreaterThan4() throws Exception {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        boolean anyNumberGreaterThan4 = numbers
+                .stream()
+                .anyMatch(number -> number > 4);
+
+        assertThat(anyNumberGreaterThan4, is(true));
+    }
+
+    @Test
+    public void shouldCheckIfEachNumberIsPair() throws Exception {
+        List<Integer> numbers = Arrays.asList(2, 4, 6);
+
+        boolean eachNumberIsPair = numbers
+                .stream()
+                .allMatch(number -> number % 2 == 0);
+
+        assertThat(eachNumberIsPair, is(true));
+    }
+
+    @Test
+    public void shouldCheckIfEachNumberIsNotPair() throws Exception {
+        List<Integer> numbers = Arrays.asList(3, 5, 7);
+
+        boolean eachNumberIsNotPair = numbers
+                .stream()
+                .noneMatch(number -> number % 2 == 0);
+
+        assertThat(eachNumberIsNotPair, is(true));
+    }
+}
